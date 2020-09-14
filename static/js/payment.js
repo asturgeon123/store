e842fcc738b1f7ae994efa4f7dff0151d94b11a8
asturgeon123 <32243510+asturgeon123@users.noreply.github.com>
2020-09-13 22:50:34 -0500

    Fuck

--- static/js/payment.js
+++ static/js/payment.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 var inputs = document.getElementsByClassName("shipping-form");
-var stripe = Stripe('pk_live_51HI3pfLOw0BgohiPif7eUuScjpMq5W3vPhcULBuAcRXkgPbOAyq71MLpPQ4vDDLmq6g6uhQlJhKandSGQzHj1GzJ00XQlMNs6z');
+//var stripe = Stripe('pk_live_51HI3pfLOw0BgohiPif7eUuScjpMq5W3vPhcULBuAcRXkgPbOAyq71MLpPQ4vDDLmq6g6uhQlJhKandSGQzHj1GzJ00XQlMNs6z');
+var stripe = Stripe('pk_test_51HI3pfLOw0BgohiPe0gDzdbSnz8yWvcEK2V1dheuuqOwciZikeRQXLtNXmO4kTaplQWdC1IhG9NxT65u3TXeYUmr00lWMmoXZ0')
 var ready = true
 var submission_status = window.sessionStorage;
 
@@ -22,12 +23,12 @@ $('#checkout_button').on('click',function(e){
 		var order = {info:{"name":$("#1_name").val(),"email":$("#1_email").val(),"phone":$("#1_phone").val(),"address":$("#1_address").val(),"city":$("#2_address").val(),"state":$("#3_address").val(),"zip":$("#4_address").val()}}
 		var counter = 0
 		var product_info = document.getElementsByClassName("product_info")
-		order["order"]["data"] = []
+		order["order"] = []
 		for(var i=0; i<product_info.length; i++) {
 			counter++
 			console.log($(product_info[i]).data('name'))
 			var name = $(product_info[i]).data('name')
-			order["order"]["data"][0].push([name,$(product_info[i]).data('title'),$(product_info[i]).data('count')]);
+			order["order"][0] = [name,$(product_info[i]).data('title'),$(product_info[i]).data('count')];
 		}
 					submission_status.setItem("status", 0);
 console.log(submission_status.getItem("status"))
